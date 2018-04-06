a9e519132216be698a6bad254aaad194587becf8
firegoods <1337343005@qq.com>
2018-04-06 11:46:31 +0800

    add

--- lss-java-tutorial-basic/src/com/lss/java/tutorial/basic/OperatorDemo.java
+++ lss-java-tutorial-basic/src/com/lss/java/tutorial/basic/OperatorDemo.java
@@ -16,6 +16,11 @@ public class OperatorDemo implements Demo{
             unaryOperator();
             //++result result++
             ProPostOperator();
+            //:?,&&,||
+            conditionalOperator();
+            //instance of
+            new OperatorDemo().instanceOfOperator();
+            new OperatorDemo().bitOperator();
         }catch (Exception e){
 
         }
@@ -137,5 +142,127 @@ public class OperatorDemo implements Demo{
         System.out.println(result++);
         //print 5
         System.out.println(result);
+    }
+
+    /**
+     * 等于、关系和条件运算符
+     */
+    public static void conditionalOperator(){
+        System.out.println("====conditionalOperator====");
+        int value1 = 1;
+        int value2 = 2;
+        if ((value1 == 1) && (value2==2)) {
+            System.out.println("(value1 == 1) && (value2==2)");
+        }
+        if ((value1 == 1) || (value2==2)){
+            System.out.println("(value1 == 1) || (value2==2)");
+        }
+
+        System.out.println("====ternaryOperator====");
+
+        boolean condition = true;
+        int result;
+        result = condition ? value1 : value2;
+        System.out.println(" result = condition ? value1 : value2: "+result);
+
+
+    }
+
+
+    /**
+     * 类型比较符
+     *keep in mind that null is not an instance of anything.
+     */
+    public  void instanceOfOperator(){
+
+        System.out.println("========instanceOfOperator=========");
+        Parent obj1 = new Parent();
+        Parent obj2 = new Child();
+        //true
+        System.out.println(obj1 instanceof Parent);
+        //false
+        System.out.println(obj1 instanceof MyInterface);
+        //false
+        System.out.println(obj1 instanceof Child);
+
+        //true
+        System.out.println(obj2 instanceof Parent);
+        //true
+        System.out.println(obj2 instanceof MyInterface);
+        //true
+        System.out.println(obj2 instanceof Child);
+
+        System.out.println(null instanceof Object);
+    }
+
+    class Parent{}
+    interface MyInterface{}
+    class Child extends Parent implements MyInterface{}
+
+
+    /**
+     *
+     *
+     *（1）机器都是使用补码，运算也是使用补码运算。
+     *（2）正数的原码补码反码都一样。
+     *（3）补码与原码相互转换，其运算过程是相同。
+     * [ +5 ] = 原码[ 0000 0101 ] = 反码[ 0000 0101 ] = 补码[ 0000 0101 ]
+     * [ -5 ] = 原码[ 1000 0101 ] = 反码[ 1111 1010 ] = 补码[ 1111 1011 ]
+     * bitwise bit shift
+     * ~,
+     * <<,>>,>>>
+     * &,^,|
+     *
+     */
+    public void bitOperator(){
+        int value1 = 0b0;
+        int value2 = 0xF;//0b01111
+
+        int value3 = -0xF;//-0b01111
+
+        int bitMask = 0x000F;
+        int value4 = 0x2222;
+
+        System.out.println("value1,value2,value3,value4: "+value1
+                +","+value2
+                +","+value3
+                +","+value4);
+
+        //-0b1
+        System.out.println("~0b0: " + ~value1);
+        //-0b10000
+        System.out.println("~0xF: " + ~value2);
+
+        //-15的原码：【1000 0000 0000 0000 0000 0000 0000 1111】
+        //-15的反码：【1111 1111 1111 1111 1111 1111 1111 0000】
+        //-15的补码：【1111 1111 1111 1111 1111 1111 1111 0001】
+
+        //-15不带符号的右移一位：【0111 1111 1111 1111 1111 1111 1111 1000】  因为最高位是正数所以补码、反码、原码一样
+
+        //-15带符号的右移一位：【1111 1111 1111 1111 1111 1111 1111 0000】
+        //-15带符号的右移一位补码转原码  先转反码【0000 0000 0000 0000 0000 0000 0000 1111】 反码+1【1000 0000 0000 0000 0000 0000 0001 0000】
+        System.out.println("-0xF>>1: " + (value3>>1));
+        //0
+        System.out.println("0b0<<1: " + (value1<<1));
+
+        System.out.println("-0xF>>1: " + (value3>>1));
+
+        System.out.println("-0xF<<1: " + (value3<<1));
+
+        System.out.println("-0xF>>>1: " + (value3>>>1));
+        System.out.println(0b01111111111111111111111111111000);
+
+        //undo
+        System.out.println("0x2222 & 0x000F = "+ (value4 & bitMask));
+
+
+
+
+
+
+
+
+
+
     }
 }
